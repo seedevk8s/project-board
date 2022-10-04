39c0a18bb3cf59d03c171cc76ed6878a89f95d01
seedevk8s <seedevk8s@gmail.com>
2022-10-04 22:55:10 +0900

    first commit

---
+++ src/main/java/kr/co/ezenac/projectboard/EzenProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ezenac.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzenProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzenProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/ezenac/projectboard/EzenProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ezenac.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzenProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
